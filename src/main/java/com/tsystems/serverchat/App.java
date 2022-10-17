f7514b9c7556d1aade78d74d23b264973117e47b
Alonso Artimez <aalonsoa@t-systems.es>
2022-10-17 10:15:23 +0200

    Created basic structure

---
+++ src/main/java/com/tsystems/serverchat/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+//Exampled https://www.codejava.net/java-se/networking/java-socket-server-examples-tcp-ip
+package com.tsystems.serverchat;
+
+/**
+ *
+ * @author aalonsoa
+ */
+public class App {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
