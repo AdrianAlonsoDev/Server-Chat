1b546d2501d5f88a5a4f4c5c9576d095fdceb24a
Maldonado Ramirez <ramaldon@t-systems.es>
2022-10-17 14:43:15 +0200

    Test

---
+++ src/test/java/com/tsystems/serverchat/ServerChatTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.tsystems.serverchat;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ramaldon
+ */
+public class ServerChatTest {
+    
+    public ServerChatTest() {
+    }
+
+    /**
+     * Test of run method, of class ServerChat.
+     */
+    @Test
+    public void testRun() throws Exception {
+        System.out.println("run");
+        ServerChat instance = new ServerChat();
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
