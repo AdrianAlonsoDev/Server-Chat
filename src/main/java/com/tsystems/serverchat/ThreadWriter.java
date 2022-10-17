bc5923889de1110a7954b2d1f887ae38601669f0
Alonso Artimez <aalonsoa@t-systems.es>
2022-10-17 14:27:42 +0200

    Added ThreadWriter class

---
+++ src/main/java/com/tsystems/serverchat/ThreadWriter.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tsystems.serverchat;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author aalonsoa
+ */
+public class ThreadWriter implements Runnable {
+
+    private ArrayList<String> messagesArray;
+    private Chat chat;
+
+    public ThreadWriter(ArrayList<String> messagesArray, Chat chat)
+    {
+        this.chat = chat;
+        this.messagesArray = messagesArray;
+    }
+
+    @Override
+    public void run()
+    {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        for (String str : messagesArray) {
+            try {
+                chat.addText(str);
+                messagesArray.remove(str);
+            } catch (IOException ex) {
+                Logger.getLogger(ThreadWriter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+}
