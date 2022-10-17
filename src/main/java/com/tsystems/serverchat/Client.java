45ed1aedb815d824a9c6b001f805b65dccdce0ba
Alonso Artimez <aalonsoa@t-systems.es>
2022-10-17 12:24:03 +0200

    Added Clinet class

---
+++ src/main/java/com/tsystems/serverchat/Client.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tsystems.serverchat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author aalonsoa
+ */
+public class Client {
+
+    public static void main(String[] args)
+    {
+        run();
+    }
+
+    public static void run()
+    {
+        String hostname = "192.168.3.112";
+        int port = 8080;
+
+        try ( Socket socket = new Socket(hostname, port)) {
+
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            Scanner in = new Scanner(System.in);
+            String message;
+
+            do {
+                System.out.println("Enter text: ");
+                message = in.nextLine();
+
+                writer.println(message);
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                String time = reader.readLine();
+
+                System.out.println(time);
+
+            } while (!message.equals("bye"));
+
+            socket.close();
+
+        } catch (UnknownHostException ex) {
+
+            System.out.println("Server not found: " + ex.getMessage());
+
+        } catch (IOException ex) {
+
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+}
