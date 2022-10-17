42882991c7c18d15ed63ff2d15518fc3d10fd110
Alonso Artimez <aalonsoa@t-systems.es>
2022-10-17 12:17:27 +0200

    Added Client test class

--- src/main/java/com/tsystems/serverchat/ServerChat.java
+++ src/main/java/com/tsystems/serverchat/ServerChat.java
@@ -11,11 +11,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -26,7 +25,8 @@ public class ServerChat {
     private ServerSocket serverSocket;
     private ArrayList<Socket> clientSock = new ArrayList<>();
 
-    public ServerChat() throws IOException {
+    public ServerChat() throws IOException
+    {
         serverSocket = new ServerSocket(PORT);
         clientSock = new ArrayList<>();
     }
@@ -34,53 +34,59 @@ public class ServerChat {
     /*
     * Starts the server
      */
-    public void run() throws IOException {
+    public void run() throws IOException
+    {
 
         while (true) {
 
+            InetAddress ia = InetAddress.getLocalHost();
+            System.out.println("ip:" + ia);
             System.out.println("Server listening for a connection");
             Socket clientSocket = serverSocket.accept();
 
             System.out.println("Received connection ");
             clientSock.add(clientSocket);
-            
+
             process(read(clientSocket));
-            
-            
+
         }
 
     }
 
     /**
      * Read from the socket the mensaje
+     *
      * @param client socket to be readed
      * @return text that has been readed from the socket
      * @throws IOException the socket can not be readed
      */
-    private String read(Socket client) throws IOException {
+    private String read(Socket client) throws IOException
+    {
         InputStream input;
-        String text="";
+        String text = "";
         try {
             input = client.getInputStream();
         } catch (IOException ex) {
             throw new IOException("Imput read socket IO Exception");
         }
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        
-        while(reader.ready()){
-            text+=reader.readLine();
+
+        while (reader.ready()) {
+            text += reader.readLine();
         }
-        
+        System.out.println(text);
         return text;
     }
 
     /**
      * Send a mensage to a expecific socket
+     *
      * @param client socket where we send the mensage
      * @param text to send to the socket
      * @throws IOException cant write in the socket
      */
-    private void write(Socket client, String text) throws IOException {
+    private void write(Socket client, String text) throws IOException
+    {
         OutputStream output;
         try {
             output = client.getOutputStream();
@@ -88,28 +94,28 @@ public class ServerChat {
             throw new IOException("Output write socket IO Exception");
         }
         PrintWriter writer = new PrintWriter(output, true);
-        
+
         writer.println(text);
         //writer.close();
     }
-    
+
     /**
      * Send a specific mensage to all my sockets
+     *
      * @param mensage to be sended
      * @throws IOException write error
      * @see write
      */
-    private void broadcastAll(String mensage) throws IOException{
+    private void broadcastAll(String mensage) throws IOException
+    {
         for (Socket socket : clientSock) {
-            write(socket,mensage);
+            write(socket, mensage);
         }
     }
 
-    
-    private void process(String read) {
+    private void process(String read)
+    {
         //SEND MESAJE TO THE CHAT
     }
-    
-    
 
 }
