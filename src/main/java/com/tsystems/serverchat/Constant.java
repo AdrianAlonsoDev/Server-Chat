9736a1c9cfe4c624179f959caa5a7ac88112a3bf
Padilla Montero <dpadilla@t-systems.es>
2022-10-17 10:44:02 +0200

    Constant class creation

---
+++ src/main/java/com/tsystems/serverchat/Constant.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tsystems.serverchat;
+
+/**
+ *
+ * @author dpadilla
+ */
+public class Constant {
+    public static int PORT=8888;
+    public static String IP= "192.168.3.112";
+    
+}
